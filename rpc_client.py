6cd3fd7f14576ab54b2097b75199fc21618fed03
Daniel Fernandes Silva <danielfs.ti@gmail.com>
2017-01-21 11:27:11 -0200

    Implementação do tutorial sobre RPC do RabbitMQ, com algumas modificações.

---
+++ rpc_client.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+import pika
+import uuid
+import time
+
+class FibonacciRpcClient(object):
+    def __init__(self):
+        self.connection = pika.BlockingConnection(
+            pika.ConnectionParameters(
+                host='rabbitmq'
+            )
+        )
+
+        self.channel = self.connection.channel()
+
+        result = self.channel.queue_declare(exclusive=True)
+        self.callback_queue = result.method.queue
+
+        self.channel.basic_consume(
+            self.on_response,
+            no_ack=True,
+            queue=self.callback_queue
+        )
+
+    def on_response(self, ch, method, props, body):
+        if self.corr_id == props.correlation_id:
+            self.response = body
+
+    def call(self, n):
+        self.response = None
+        self.corr_id = str(uuid.uuid4())
+        self.channel.basic_publish(
+            exchange='',
+            routing_key='rpc_queue',
+            properties=pika.BasicProperties(
+                reply_to=self.callback_queue,
+                correlation_id=self.corr_id
+            ),
+            body=str(n)
+        )
+
+        while self.response is None:
+            self.connection.process_data_events()
+
+        return int(self.response)
+
+
+fibonacci_rpc = FibonacciRpcClient()
+
+print(" Waiting 5 seconds to start...")
+time.sleep(5)
+
+for n in range(1, 10001):
+    print(" [x] Requesting fib(%r)" % n)
+    response = fibonacci_rpc.call(n)
+    print(" [.] Got %r" % response)
---
+++ rpc_server.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+import pika
+from fastfibonacci.fastfibonacci import fibonacci
+
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(
+        host='rabbitmq'
+    )
+)
+
+channel = connection.channel()
+channel.queue_declare(queue='rpc_queue')
+
+
+def fib(n):
+    if n == 0 or n == 1:
+        return n
+    else:
+        return fib(n - 1) + fib(n - 2)
+
+
+def on_request(ch, method, props, body):
+    n = int(body)
+
+    print(" [.] fib(%s)" % n)
+    response = fibonacci(n)
+
+    ch.basic_publish(
+        exchange='',
+        routing_key=props.reply_to,
+        properties=pika.BasicProperties(
+            correlation_id=props.correlation_id
+        ),
+        body=str(response)
+    )
+    ch.basic_ack(
+        delivery_tag=method.delivery_tag
+    )
+
+
+channel.basic_qos(prefetch_count=1)
+channel.basic_consume(on_request, queue='rpc_queue')
+
+print(" [x] Awaiting RPC requests")
+channel.start_consuming()
